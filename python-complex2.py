897bf13acd4f573ae29c7c7d90e4f59e35fbbf25
Logan Stoll <Loganstoll123@gmail.com>
2017-10-23 14:39:35 -0400

    update

--- python-complex2.py
+++ python-complex2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-#Lstoll
-#Complex 2 (10/18/17)
+#LoganStoll123
+#Complex 2 (10/23/17)
 
 import math
 pi = 3.1415926535897932384626433
@@ -49,26 +49,44 @@ def magnitude(number):
     absolute = math.sqrt((number[0] * number[0]) + (number[1]* number[1]))
     return absolute
 
-# Getting the basic information from the user.
-
-# coordinate_system = raw_input('Greetings User! Shall you be working in rectangular or polar format today?:\n')
-print('For this particular experiment, I (the pi) am only capable of series AC calculations with one R, L & C.\n')
+# coordinate_system = raw_input
 frequency = input('\nWhat is the frequency of the source? (Hz): ')
 voltage = input('\nWhat is the voltage of the source? (RMS): ')
 resistor_value = input('\nWhat value of resistor is present? (Ohms): ')
 inductor_value = input('\nWhat is the value of your inductor? (Henrys): ')
 capacitor_value = input('\nWhat is the value of your capacitor? (Farads): ')
+inductor_resistance = input('\nWhat is the resistance of the wiring of the inductor? ( Ohms): ')
 
-# Some basic calculations
-omega = 2 * pi * frequency
-inductance = omega * inductor_value
-capacitance = (1/(omega * capacitor_value))
+# calculations
+total_resistance = inductor_resistance + resistor_value
+inductance = 2 * pi * frequency * inductor_value
+capacitance = (1/(2 * pi * frequency * capacitor_value))
 impedance = resistor_value, (inductance + -capacitance)
 mag_impedance = magnitude(impedance)
+mag_inductance = (inductor_resistance, inductance)
+mag_inductance = magnitude(mag_inductance)
 current = float(voltage) / float(mag_impedance)
 v_r = current * resistor_value
 v_l = current * inductance
 v_c = current * capacitance
+
+# theta 
+if inductance > capacitance:
+    argument_send = impedance[1] / impedance[0]
+else:
+    if capacitance > inductance:
+        argument_send = impedance[0] / impedance[1]
+    else:
+        argument_send = 0
+
+phase_radians = math.atan(argument_send)
+phase_angle = phase_radians * 180/pi
+
+if inductance > capacitance:
+    print('Your current will lag your voltage by %f degrees' % phase_angle)
+if capacitance > inductance:
+    print('Your current will lead your voltage by %f degrees ' % phase_angle)    
+    
 print('\nYour total impedance is: %.2f + %.2fj' % (impedance[0], impedance[1]))
 print('That means the magnitude of your impedance is: %.2f' % mag_impedance)
 print('Which then means your current is: %f A' % current)
